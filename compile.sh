0d89cdb931895864cc899c8612f71d330bc0b3e6
Indy <indy@pop-os.localdomain>
2023-04-30 10:22:05 -0400

    commit removing the install of flatpak apps

--- compile.sh
+++ compile.sh
@@ -106,6 +106,3 @@ rm setup.sh
 
 # Getting the flathub repo
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-
-# Flatpak Install
-flatpak install com.github.tchx84.Flatseal com.brave.Browser com.bitwarden.desktop net.brinkervii.grapejuice org.prismlauncher.PrismLauncher xyz.xclicker.xclicker com.github.unrud.VideoDownloader 
