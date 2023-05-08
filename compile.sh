0dcec17ed54366fc653e7eac6d52da0acb44d4a2
Indy <indy@pop-os.localdomain>
2023-05-07 20:23:18 -0400

    doing some big changes

--- compile.sh
+++ compile.sh
@@ -1,13 +1,18 @@
+
 #!/bin/env bash
 
 # Script Variables
 builddir=$(pwd)
 
-# Making directories & files 
-mkdir -p ~/Github ~/Img ~/Applications ~/Zsh/Plugins ~/Pictures/Screenshots ~/Scripts ~/.icons ~/.themes ~/.cache/zsh ~/.local/bin ~/Desktop ~/Documents ~/Downloads ~/Music ~/Pictures ~/Public ~/Videos ~/.config
+echo "########################################"
+echo "## Adding Some Directories, And Files ##"
+echo "########################################"
+mkdir -pv ~/Github ~/Img ~/Applications ~/Zsh/Plugins ~/Pictures/Screenshots ~/Scripts ~/.icons ~/.themes ~/.cache/zsh ~/.local/bin ~/Desktop ~/Documents ~/Downloads ~/Music ~/Pictures ~/Public ~/Videos
 touch ~/.cache/zsh/history 
 
-# Shell Color Scripts
+echo "#########################"
+echo "## Shell Color Scripts ##"
+echo "#########################" 
 cd ~/Github
 git clone https://gitlab.com/dwt1/shell-color-scripts.git
 cd shell-color-scripts
@@ -15,7 +20,9 @@ sudo make install
 sudo cp completions/_colorscript /usr/share/zsh/site-functions
 cd ~
 
-# Rofi Calc
+echo "################"
+echo "## Rofi Emoji ##"
+echo "################" 
 cd ~/Github
 git clone https://github.com/Mange/rofi-emoji.git
 cd rofi-emoji
@@ -27,7 +34,9 @@ make
 sudo make install
 cd ~
 
-# Rofi Emoji
+echo "###############"
+echo "## Rofi Calc ##"
+echo "###############" 
 cd ~/Github
 git clone https://github.com/svenstaro/rofi-calc.git
 cd rofi-calc
@@ -39,76 +48,107 @@ make
 make install
 cd ~
 
-# Installing moar pager
-wget https://github.com/walles/moar/releases/download/v1.13.0/moar-v1.13.0-linux-386 
-chmod a+x moar-*-*-*
+echo "################"
+echo "## Moar Pager ##"
+echo "################" 
+wget -q https://github.com/walles/moar/releases/download/v1.13.0/moar-v1.13.0-linux-386 
+chmod -v a+x moar-*-*-*
 mv moar-*-*-* ~/.local/bin/moar
 
-# Installing go stuff
-go install github.com/jesseduffield/lazygit@latest github.com/xxxserxxx/gotop/v4/cmd/gotop@latest
+echo "###########"
+echo "## GetNF ##"
+echo "###########" 
+cd ~/Github
+git clone https://github.com/ronniedroid/getnf.git
+cd getnf
+./install.sh
+cd ~
 
-# Rustup
+echo "#################"
+echo "## Go Programs ##"
+echo "#################" 
+go install github.com/charmbracelet/glow@latest github.com/jesseduffield/lazygit@latest github.com/xxxserxxx/gotop/v4/cmd/gotop@latest
+
+echo "##################"
+echo "## Rust Install ##"
+echo "##################" 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
-# Moving And Deletig And Adding Files
+echo "########################################"
+echo "## Moving, Deleting, And Adding Files ##"
+echo "########################################" 
 cd $builddir
 git clone https://github.com/indyleo/Wallpapers.git ~/Pictures/Wallpapers/
-mv autostart-scripts sxhkd polybar Thunar neofetch nvim ranger picom awesome kitty rofi starship.toml mimeapps.list user-dirs.dirs greenclip.toml ~/.config/
+mv  autostart-scripts sxhkd polybar Thunar neofetch nvim ranger picom awesome conky kitty rofi starship.toml mimeapps.list user-dirs.dirs greenclip.toml ~/.config/
 rm ~/.bashrc ~/.profile ~/.zshenv
 mv .bashrc .zshrc .zshenv .aliasrc .xsession .profile .xinitrc .Xresources ~/
 mv $builddir/scripts/wifimenu $builddir/scripts/powermenu ~/.local/bin/
 
-# Zsh Autopairs
+echo "#################"
+echo "## Zsh Plugins ##"
+echo "#################" 
 cd ~/Zsh/Plugins
 git clone https://github.com/hlissner/zsh-autopair.git
 cd ~ 
 
-# Zsh You Should Use
 cd ~/Zsh/Plugins
 git clone https://github.com/MichaelAquilina/zsh-you-should-use.git
 cd ~ 
 
-# Zsh Completions
 cd ~/Zsh/Plugins
 git clone https://github.com/zsh-users/zsh-completions.git
 cd ~
 
-# Zsh History Substring Search
 cd ~/Zsh/Plugins
 git clone https://github.com/zsh-users/zsh-history-substring-search.git
 cd ~
 
-# Nordzy Cursors
+echo "###################"
+echo "## Cursors Theme ##"
+echo "###################" 
 cd ~/Github
 git clone https://github.com/alvatip/Nordzy-cursors.git
 cd Nordzy-cursors
 ./install.sh
 
-# Papirus Icon Theme
+echo "#################"
+echo "## Icons Theme ##"
+echo "#################" 
 wget -qO- https://git.io/papirus-icon-theme-install | DESTDIR="$HOME/.icons" sh
 
-# Nordic Theme
+echo "###############"
+echo "## GTK Theme ##"
+echo "###############" 
 cd ~/.themes
 git clone https://github.com/EliverLara/Nordic.git
 
-# Rofi Power Menu
+echo "################"
+echo "## Power Menu ##"
+echo "################"
 cd ~/Github
 git clone https://github.com/jluttine/rofi-power-menu.git
 cd rofi-power-menu
 cp rofi-power-menu ~/.local/bin/
 cd ~
 
-# Greenclip
+echo "###############"
+echo "## Greenclip ##"
+echo "###############"
 cd ~/.local/bin
 wget https://github.com/erebe/greenclip/releases/download/v4.2/greenclip
 chmod a+x greenclip 
 cd ~
 
-# Nitch
+echo "###########"
+echo "## Nitch ##"
+echo "###########"
 wget https://raw.githubusercontent.com/unxsh/nitch/main/setup.sh
 chmod a+x setup.sh
 ./setup.sh
 rm setup.sh
 
-# Getting the flathub repo
+echo "##################"
+echo "## Flatpak Repo ##"
+echo "##################"
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
